d0b3cd0ef205ac598a776e363f42c46f22c05fe1
unknown <name.p.0001@gmail.com>
2026-06-28 00:02:54 +0330

    Day 1: age calculator completed

---
+++ day1/Main.py
@@ -0,0 +1,5 @@
+user_name = input("What is your name?:")
+user_birth_year = int(input("What is your birth year?:"))
+current_year = 2026
+age = current_year - user_birth_year
+print (f"{user_name} you are currently {age} years old.")
