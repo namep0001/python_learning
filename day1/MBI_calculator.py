869fbaa72b8f36d23f2f55e7583063e60ff27108
namep0001 <name.p.0001@gmail.com>
2026-06-28 15:11:50 +0330

    day1: added MBI_calculator.py / small fixes

--- day1/MBI_calculator.py
+++ day1/MBI_calculator.py
@@ -3,13 +3,13 @@ print("Welcome to BMI Calculator")
 
 height = float(input("What is your height?(M):"))
 weight = float(input("What is your weight?(KG):"))
-BMI = weight / (height * height)
+bmi = weight / (height * height)
 
-if(BMI <= 18.5):
-    print(f"Underweight {BMI}")
-elif(18.5 <= BMI <= 24.9):
-    print(f"Healthy Weight {BMI}")
-elif(25.0 <= BMI <= 29.9):
-    print(f"Overweight {BMI}")
-elif(BMI >= 30.0):
-    print(f"Obese {BMI}")
+if(bmi <= 18.5):
+    print(f"Underweight {bmi}")
+elif(18.5 <= bmi <= 24.9):
+    print(f"Healthy Weight {bmi}")
+elif(25.0 <= bmi <= 29.9):
+    print(f"Overweight {bmi}")
+else:
+    print(f"Obese {bmi}")
