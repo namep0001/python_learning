b6b0f33c64d786d84f16665ca3fbe2e1f39611f7
namep0001 <name.p.0001@gmail.com>
2026-06-28 18:16:04 +0330

    added 2 decimal

--- day1/unit_converter.py
+++ day1/unit_converter.py
@@ -11,26 +11,26 @@ number_to_convert = float(input("Enter the number you want to convert:"))
 if(choice == 1):
     print("1- km to mile.")
     miles_conv = number_to_convert * 0.6213711922
-    print(f"{number_to_convert} km is {miles_conv} mile.")
+    print(f"{number_to_convert} km is {miles_conv:.2f} mile.")
 elif(choice == 2):
     print("2- miles to km.")
     km_conv = number_to_convert * 1.609344
-    print(f"{number_to_convert} mile is {km_conv} km.")
+    print(f"{number_to_convert} mile is {km_conv:.2f} km.")
 elif(choice == 3):
     print("3- kg to lbs.")
     lbs_conv = number_to_convert * 2.2046226218
-    print(f"{number_to_convert} kg is {lbs_conv} pound/lbs.")
+    print(f"{number_to_convert} kg is {lbs_conv:.2f} pound/lbs.")
 elif(choice == 4):
     print("4- lbs to kg.")
     kg_conv = number_to_convert * 0.453592
-    print(f"{number_to_convert} pound/lbs is {kg_conv} kg.")
+    print(f"{number_to_convert} pound/lbs is {kg_conv:.2f} kg.")
 elif(choice == 5):
     print("5- centimetre to inch.")
     inch_conv = number_to_convert * 0.3937007874
-    print(f"{number_to_convert} centimetre is {inch_conv} inch.")
+    print(f"{number_to_convert} centimetre is {inch_conv:.2f} inch.")
 elif(choice == 6):
     print("6- inch to centimetre.")
     centimetre_conv = number_to_convert * 2.54
-    print(f"{number_to_convert} inch is {centimetre_conv} centimetre.")
+    print(f"{number_to_convert} inch is {centimetre_conv:.2f} centimetre.")
 else:
     print("Invalid choice.")
