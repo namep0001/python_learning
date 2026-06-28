e8a6f17310a39b7fc17990dd560e4fff5f51d1e7
namep0001 <name.p.0001@gmail.com>
2026-06-28 18:19:03 +0330

    small fix

--- day1/bmi_calculator.py
+++ day1/bmi_calculator.py
@@ -5,7 +5,7 @@ height = float(input("What is your height?(M):"))
 weight = float(input("What is your weight?(KG):"))
 bmi = weight / (height * height)
 
-if(bmi <= 18.5):
+if(bmi < 18.5):
     print(f"Underweight {bmi:.2f}")
 elif(18.5 <= bmi <= 24.9):
     print(f"Healthy Weight {bmi:.2f}")
