558f229c22983ae3b7cdee60575d697ba580c7c4
namep0001 <name.p.0001@gmail.com>
2026-06-28 18:33:23 +0330

    Bill_splitter.py fixes

--- day1/bill_splitter.py
+++ day1/bill_splitter.py
@@ -1,6 +1,12 @@
 # variables / input
 total_bill = float(input("Enter the total amount of bill:"))
+while(total_bill <= 0):
+    print("Invalid Input")
+    total_bill = int(input("Enter the total amount of bill again:"))
 people_number = int(input("How many people want to split the bill?:"))
+while(people_number <= 0):
+    print("Invalid Input")
+    people_number = int(input("Enter the number of people again:"))
 tip_percentage = int(input("Tips percentage:"))
 
 # compute
@@ -9,4 +15,4 @@ grand_total = total_bill + tip_amount
 fair_share = grand_total / people_number
 
 # output
-print (f"Tip Amount : {tip_amount:.2f} $ \n Total : {grand_total:.2f} $ \n Number of people : {people_number}\nEach person's share : {fair_share:.2f} $")
+print(f"Tip Amount : {tip_amount:.2f} $ \n Total : {grand_total:.2f} $ \n Number of people : {people_number}\nEach person's share : {fair_share:.2f} $")
