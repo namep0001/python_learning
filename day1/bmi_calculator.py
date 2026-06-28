6b9c947c0519a46960dbaf003148eadc21134a2e
namep0001 <name.p.0001@gmail.com>
2026-06-28 18:41:48 +0330

    small fix

--- day1/bmi_calculator.py
+++ day1/bmi_calculator.py
@@ -9,7 +9,7 @@ if(bmi < 18.5):
     print(f"Underweight {bmi:.2f}")
 elif(18.5 <= bmi <= 24.9):
     print(f"Healthy Weight {bmi:.2f}")
-elif(25.0 <= bmi <= 29.9):
+elif(24.9 < bmi <= 29.9):
     print(f"Overweight {bmi:.2f}")
 else:
     print(f"Obese {bmi:.2f}")
