d0f8a09dd9d8ebd71f7e1cf23b3247742816d9b5
namep0001 <name.p.0001@gmail.com>
2026-06-28 18:12:42 +0330

    added 2 decimal

--- day1/bmi_calculator.py
+++ day1/bmi_calculator.py
@@ -6,10 +6,10 @@ weight = float(input("What is your weight?(KG):"))
 bmi = weight / (height * height)
 
 if(bmi <= 18.5):
-    print(f"Underweight {bmi}")
+    print(f"Underweight {bmi:.2f}")
 elif(18.5 <= bmi <= 24.9):
-    print(f"Healthy Weight {bmi}")
+    print(f"Healthy Weight {bmi:.2f}")
 elif(25.0 <= bmi <= 29.9):
-    print(f"Overweight {bmi}")
+    print(f"Overweight {bmi:.2f}")
 else:
-    print(f"Obese {bmi}")
+    print(f"Obese {bmi:.2f}")
