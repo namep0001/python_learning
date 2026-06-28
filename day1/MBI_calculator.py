f70efc2ff3034e0f79d258bed03e0c2153d59337
namep0001 <name.p.0001@gmail.com>
2026-06-28 15:00:29 +0330

    small_fix

--- day1/MBI_calculator.py
+++ day1/MBI_calculator.py
@@ -5,11 +5,11 @@ height = float(input("What is your height?(M):"))
 weight = float(input("What is your weight?(KG):"))
 BMI = weight / (height * height)
 
-if(BMI < 18.5):
+if(BMI <= 18.5):
     print(f"Underweight {BMI}")
 elif(18.5 <= BMI <= 24.9):
     print(f"Healthy Weight {BMI}")
 elif(25.0 <= BMI <= 29.9):
     print(f"Overweight {BMI}")
-elif(BMI > 30.0):
+elif(BMI >= 30.0):
     print(f"Obese {BMI}")
