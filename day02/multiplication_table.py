30bb9b3ce4617f8dc06ece5b2c362a5e0067c0aa
namep0001 <name.p.0001@gmail.com>
2026-06-30 17:10:39 +0330

    Exercise 4.5 — Mini Calculator inside multiplication_table.py

--- day02/multiplication_table.py
+++ day02/multiplication_table.py
@@ -1,6 +1,22 @@
 # multiplication_table.py
 
 number = int(input("Enter a number:"))
+# if number is != int it gaves error/ program crash
 
 for i in range (1, 10):
     print(f"{number} * {i} = " , number * i)
+
+print("Do you want to enter another number? Y/N")
+answer = str(input("Y for yes , N for no :"))
+while(answer == "Y"):
+    number = int(input("Enter a number:"))
+    for i in range (1, 10):
+        print(f"{number} * {i} = " , number * i)
+    print("Do you want to enter another number? Y/N")
+    answer = str(input("Y for yes , N for no :"))
+
+if(answer == "N"):
+    print("Closing the program ...")
+elif (answer != "Y" or answer !="N"):
+    print("Invalid Input.")
+    print("Closing the program ...")
