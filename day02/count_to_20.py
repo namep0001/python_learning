c9e076bf33b10ef63866bf9097064eb74d14596f
namep0001 <name.p.0001@gmail.com>
2026-06-30 12:11:32 +0330

    Exercise 1: count_to_20.py

---
+++ day02/count_to_20.py
@@ -0,0 +1,11 @@
+# count_to_20.py
+
+# for
+for count_for in range(1,21):
+    print (count_for)
+
+# while
+count_while = 1
+while (count_while <= 20):
+    print (count_while)
+    count_while = count_while + 1
