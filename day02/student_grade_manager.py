559c3a2d7868b217f8b72b9b3a185dfa8df7e471
namep0001 <name.p.0001@gmail.com>
2026-06-30 17:44:25 +0330

    added student_grade_manager.py

---
+++ day02/student_grade_manager.py
@@ -0,0 +1,21 @@
+# student_grade_manager.py
+
+print("Student Grade Manager Program Menu.")
+print("please choose one option(1,2):\n 1- Add grade \n 2- Exit")
+number_of_student = 0
+total = 0
+choice = int(input(""))
+if( 0 > choice > 2):
+    choice = int(input("Invalid input, try again:"))
+
+elif( choice == 1):
+    number_of_student = int(input("Please enter the number of students:"))
+    for i in range (number_of_student):
+        grade = float(input("Add grade:"))
+        total = total + grade
+
+    average = total / number_of_student
+    print(f"{average:.2f}")
+
+else:
+    print("Closing the program ...")
