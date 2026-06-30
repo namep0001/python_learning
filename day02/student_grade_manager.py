48061494a210876a215c05d82be3b709e96ae971
namep0001 <name.p.0001@gmail.com>
2026-06-30 21:03:24 +0330

    improve code

--- day02/student_grade_manager.py
+++ day02/student_grade_manager.py
@@ -4,11 +4,11 @@ print("Student Grade Manager Program Menu.")
 print("please choose one option(1,2):\n 1- Add grade \n 2- Exit")
 number_of_student = 0
 total = 0
-choice = int(input(""))
-if( 0 > choice > 2):
-    choice = int(input("Invalid input, try again:"))
+choice = 0
+while( 1 > choice or choice > 2):
+    choice = int(input("Your choice:"))
 
-elif( choice == 1):
+if( choice == 1):
     number_of_student = int(input("Please enter the number of students:"))
     for i in range (number_of_student):
         grade = float(input("Add grade:"))
